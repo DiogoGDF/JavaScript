fae5ca4012ed3899b4fe78b6604bef72b1a818bb
Diogo Garbinato <diogogarbinato@gmail.com>
2023-04-16 16:37:49 -0300

    Updates to html and js files

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,20 +1,23 @@
-let firstCard = getRandomNumber();
-let secondCard = getRandomNumber();
+let cards = [];
 let count = 0;
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let cardBtn = document.getElementById("card-btn");
 
 function getRandomNumber(){
-    return Math.floor(Math.random() * (11 - 2 + 1)) + 2;
+    return Math.floor(Math.random() * 10) + 2;
 }
 
 function startGame(){ 
+    isAlive = true;
+    cards[0] = getRandomNumber();
+    cards[1] = getRandomNumber();
+    sum = cards[0] + cards[1];
     cardsEl.textContent = "Cards: " + cards[count++] + " " + cards[count++];
     renderGame();
 }
@@ -29,14 +32,19 @@ function renderGame() {
     } else {
         message = "You're out of the game!";
         isAlive = false;
+        cardBtn.textContent = "Game Ended!";
     }
     messageEl.textContent = message;
 }
 
 function newCard(){
-    let card = getRandomNumber();
-    cards.push(card);
-    sum += card;
-    renderGame();
-    cardsEl.textContent += " " + cards[count++];
+    if(isAlive && !hasBlackJack){
+        let card = getRandomNumber();
+        cards.push(card);
+        sum += card;
+        renderGame();
+        cardsEl.textContent += " " + cards[count++];
+    } else {
+        cardBtn.textContent = "You have to start a new game first!";
+    }
 }
