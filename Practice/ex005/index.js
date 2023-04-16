fd0eac250fb2c29d998c5feefdd207de45e4b952
Diogo Garbinato <diogogarbinato@gmail.com>
2023-04-16 18:36:25 -0300

    Created ex005

---
+++ Practice/ex005/index.js
@@ -0,0 +1,16 @@
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+function pickFighter(){
+    let index = Math.floor(Math.random() * fighters.length)
+    return fighters[index]
+}
+
+fightButton.addEventListener("click", function() {
+    // Challenge:
+    // When the user clicks on the "Pick Fighters" button, pick two random 
+    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    stageEl.textContent = pickFighter() + " vs " + pickFighter()
+})
