1cdef0a4238c90451349e87a0ba24aee6aa4514a
Diogo Garbinato <diogogarbinato@gmail.com>
2023-04-17 11:36:45 -0300

    Updates

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -1,8 +1,15 @@
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 let myLeads = []
+let count = 0
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    //console.log(myLeads)
+    list()
 })
+
+function list(){
+    ulEl.innerHTML += "<li>"+myLeads[count++]+"</li>"
+}
