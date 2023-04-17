2240b718c9d52f6288282d77ca60ccc033d332fc
Diogo Garbinato <diogogarbinato@gmail.com>
2023-04-17 20:20:46 -0300

    Final updates of V1

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -1,4 +1,4 @@
-const inputBtn = document.getElementById("input-btn")
+const tabBtn = document.getElementById("tab-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
@@ -12,11 +12,17 @@ if(leadsFromLocalStorage){
     list()
 }
 
-inputBtn.addEventListener("click", addToList)
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        addToList(tabs[0].url)
+    })
+
+})
 inputEl.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
     event.preventDefault();
-    addToList();
+    const usrInput = inputEl.value
+    addToList(usrInput);
   }
 });
 
@@ -36,13 +42,12 @@ deleteBtn.addEventListener("click", function(){
     }
 })
 
-function addToList(){
+function addToList(newItem){
     let listItem
-    const usrInput = inputEl.value
-    if (usrInput.startsWith("http") || usrInput.startsWith("https") || usrInput.startsWith("www.")) {
-        listItem = `<li><a href='${usrInput}' target='_blank' rel='external'>${usrInput}</a></li>`
+    if (newItem.startsWith("http") || newItem.startsWith("https") || newItem.startsWith("www.")) {
+        listItem = `<li><a href='${newItem}' target='_blank' rel='external'>${newItem}</a></li>`
     } else {
-        listItem = `<li>${usrInput}</li>`
+        listItem = `<li>${newItem}</li>`
     }
     myLeads.push(listItem)
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
