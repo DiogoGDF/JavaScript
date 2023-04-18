7f38cd53a8766f9b06b07520f1a8941dfe051f82
Diogo Garbinato <diogogarbinato@gmail.com>
2023-04-18 19:58:44 -0300

    Major upgrades to JS
    - Added enter input with return key functionality
    - You can now remove items from the list and database by double clicking them
    - Some code refactor

--- MobileShopCart/index.js
+++ MobileShopCart/index.js
@@ -1,5 +1,5 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-3c2e8-default-rtdb.firebaseio.com/"
@@ -19,27 +19,46 @@ function clearInputFieldEl(){
 }
 
 function appendItemToShoppingListEl(itemValue){
-    //shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+    let itemId = itemValue[0]
+    let itemName = itemValue[1]
     let newEl = document.createElement("li")
-    newEl.textContent = itemValue[1]
+    newEl.textContent = itemName
     shoppingListEl.append(newEl)
+
+    newEl.addEventListener("dblclick", function(){
+        let exactLocationOfStoryInDB = ref(database, `shoppingList/${itemId}`)
+        remove(exactLocationOfStoryInDB)
+    })
 }
 
 function clearList(){
     shoppingListEl.innerHTML = ""
 }
 
-addButtonEl.addEventListener("click", function() {
+function addFromInput(){
     let inputValue = inputFieldEl.value
     push(shoppingListInDB, inputValue)
     clearInputFieldEl()
+}
+
+addButtonEl.addEventListener("click", addFromInput)
+inputFieldEl.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault()
+    //const usrInput = inputEl.value
+    addFromInput()
+  }
 })
 
 onValue(shoppingListInDB, function(snapshot){
-    let items = Object.entries(snapshot.val())
-    clearList()
-    for(let i = 0; i < items.length; i++){
-        let currentItem = items[i]
-        appendItemToShoppingListEl(currentItem)
+    if (snapshot.exists()){
+        let items = Object.entries(snapshot.val())
+        clearList()
+        for(let i = 0; i < items.length; i++){
+            let currentItem = items[i]
+            appendItemToShoppingListEl(currentItem)
+        }
+    }else{
+        clearList()
     }
 })
