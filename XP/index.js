be3ad204283235179e51e259ac5bd98f3c08ab53
Diogo Garbinato <diogogarbinato@gmail.com>
2023-04-19 15:59:28 -0300

    XP app sketch

---
+++ XP/index.js
@@ -0,0 +1,17 @@
+const progressBar = document.querySelector('.progress-bar');
+const levelEl = document.getElementById("level");
+const testEl = document.getElementById("test");
+
+function updateProgressBar(percentage) {
+  progressBar.style.width = percentage + '%';
+}
+
+let lvl = 20;
+let xp = 154;
+let totLvl = 1000;
+let percentage = xp/totLvl;
+percentage = percentage * 100;
+updateProgressBar(percentage); 
+
+levelEl.textContent = `Level: ${lvl} (${xp}/${totLvl})`;
+testEl.textContent = i;
