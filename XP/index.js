f2a620dcd7d69f7109fac9e9309ff54f0aa25743
Diogo Garbinato <diogogarbinato@gmail.com>
2023-04-24 09:56:48 -0300

    Added basic DataBase functions

--- XP/index.js
+++ XP/index.js
@@ -1,3 +1,11 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+const appSettings = {databaseURL: "https://playground-c1cb0-default-rtdb.firebaseio.com/"};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const historyInDB = ref(database, "historyXP");
+
 const progressBar = document.querySelector('.progress-bar');
 const levelEl = document.getElementById("level-el");
 const testEl = document.getElementById("test");
@@ -22,10 +30,15 @@ levelEl.textContent = `Level: ${lvl} (${xp}/${totLvl})`;
 function cadastrar(){
   const atividadeStr = atividadeEl.value;
   const xpStr = xpEl.value;
-  const str = `<li>${atividadeStr} | XP:${xpStr}</li>`;
+  const currentDate = new Date();
+  const options = {day: '2-digit', month: '2-digit', year: '2-digit'};
+  const dateInFormat = currentDate.toLocaleString('en-GB', options);
+  const str = `<li>${dateInFormat}: ${atividadeStr} | XP:${xpStr}</li>`;
+  const strDb = `${dateInFormat}: ${atividadeStr} | XP:${xpStr}`;
   atividadeEl.value = "";
   xpEl.value = "";
-  historico.innerHTML += str
+  historico.innerHTML += str;
+  push(historyInDB, strDb)
 }
 
 log.addEventListener("click", function(event){
@@ -38,3 +51,29 @@ xpEl.addEventListener("keypress", function(event) {
     cadastrar();
   }
 });
+
+function appendItemToHistory(itemValue){
+    let itemId = itemValue[0]
+    let itemName = itemValue[1]
+    let newEl = document.createElement("li")
+    newEl.textContent = itemName
+    historico.innerHTML += newEl;
+
+    newEl.addEventListener("dblclick", function(){
+        let exactLocationOfStoryInDB = ref(database, `shoppingList/${itemId}`)
+        remove(exactLocationOfStoryInDB)
+    })
+}
+
+onValue(historyInDB, function(snapshot){
+    if (snapshot.exists()){
+        let items = Object.entries(snapshot.val())
+        clearList()
+        for(let i = 0; i < items.length; i++){
+            let currentItem = items[i]
+            appendItemToHistory(currentItem)
+        }
+    }else{
+        clearList()
+    }
+})
