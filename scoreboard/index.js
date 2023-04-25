c46524a54b67eef678d15cf8ac560413bf6cd7cf
Diogo Garbinato <diogogarbinato@gmail.com>
2023-04-25 18:32:47 -0300

    new game button

--- scoreboard/index.js
+++ scoreboard/index.js
@@ -2,3 +2,8 @@ function plus(n, which){
     let element = document.getElementById(which);
     element.textContent = (n + parseInt(element.textContent));
 }
+
+function reset(){
+    document.getElementById('home').textContent = 0
+    document.getElementById('guest').textContent = 0
+}
