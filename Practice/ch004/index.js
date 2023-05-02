d0b73c158759b669f2a6ebee8b822b9e5169c6b2
Diogo Garbinato <diogogarbinato@gmail.com>
2023-05-02 15:40:29 -0300

    ch004

---
+++ Practice/ch004/index.js
@@ -0,0 +1,18 @@
+const button = document.getElementByType("button")
+
+let data = [
+    {
+        player: "Jane",
+        score: 52
+    },
+    {
+        player: "Mark",
+        score: 41
+    }
+]
+
+
+
+// Fetch the button from the DOM, store it in a variable
+// Use addEventListener() to listen for button clicks
+// Log Jane's score when the button is clicked (via data)
