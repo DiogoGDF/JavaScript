ffd186f8798d626ee9e9427a841f1aa86a8b88e9
Diogo Garbinato <diogogarbinato@gmail.com>
2023-05-02 17:17:28 -0300

    Ch004

--- Practice/ch004/index.js
+++ Practice/ch004/index.js
@@ -1,4 +1,4 @@
-const button = document.getElementByType("button")
+const btn = document.getElementById("btn")
 
 let data = [
     {
@@ -11,8 +11,9 @@ let data = [
     }
 ]
 
-
-
+btn.addEventListener("click", function(){
+    console.log(data[0].score)
+})
 // Fetch the button from the DOM, store it in a variable
 // Use addEventListener() to listen for button clicks
 // Log Jane's score when the button is clicked (via data)
