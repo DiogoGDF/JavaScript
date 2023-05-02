dff94b2ab42bbe5fd1190e7ebd928b83381d0846
Diogo Garbinato <diogogarbinato@gmail.com>
2023-05-02 12:08:23 -0300

    Create index.js

---
+++ Practice/ch003/index.js
@@ -0,0 +1,5 @@
+//save a value to localStorage
+//localStorage.setItem("Eai", "Hey.com");
+//delete your code and refresh the page
+//fetch your value from localStorage and log it out
+console.log("hey")
