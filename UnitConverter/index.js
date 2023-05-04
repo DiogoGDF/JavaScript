bc23a9b426789fbd3f7d11954ebca027c243815f
Diogo Garbinato <diogogarbinato@gmail.com>
2023-05-04 11:14:56 -0300

    HTML & CSS done

--- UnitConverter/index.js
+++ UnitConverter/index.js
@@ -3,3 +3,8 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+const btn = document.getElementById("btn")
+const length = document.getElementById("length")
+const volume = document.getElementById("volume")
+const mass = document.getElementById("mass")
+
