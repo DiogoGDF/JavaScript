78092d7499e03aaba1ad56ddde7694620feb37dc
Diogo Garbinato <diogogarbinato@gmail.com>
2023-05-05 20:46:06 -0300

    Database

--- WeAreTheChampios/index.js
+++ WeAreTheChampios/index.js
@@ -1,1 +1,11 @@
-// javascript
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-c1cb0-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const endorsmentsDB = ref(database, "endorsments")
+
