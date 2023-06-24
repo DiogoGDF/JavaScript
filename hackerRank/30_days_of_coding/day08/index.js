303c2bb13d7eab36658f276e754f9076043e40d9
DiogoGDF <diogogarbinato@gmail.com>
2023-06-24 16:34:41 -0300

    day 08

---
+++ hackerRank/30_days_of_coding/day08/index.js
@@ -0,0 +1,29 @@
+function processData(input) {
+    //Enter your code here
+    const hashMap = new Map()
+    let str = input.split("\n")
+    let n = Number(str[0])
+    let i = 1
+    for (let j = 0; j < n; j++) {
+        let line = str[i++].split(" ")
+        hashMap.set(line[0], line[1])
+    }
+    for (let j = 0; j < n; j++) {
+        if (hashMap.has(str[i]))
+            console.log(`${str[i]}=${hashMap.get(str[i])}`)
+        else
+            console.log("Not found")
+        i++
+    }
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
