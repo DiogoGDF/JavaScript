8883c24c3bf4e266074cc89b9d88e3cd3d53666c
Diogo Garbinato <diogogarbinato@gmail.com>
2023-09-16 22:26:30 -0300

    Integrando a IA (Inteligencia Artificial)

--- nlw/nlwia/server/transcribe.js
+++ nlw/nlwia/server/transcribe.js
@@ -1,4 +1,22 @@
-import { transcriptionExample } from "./utils/transcription.js"
-export async function transcribe() {
-  return transcriptionExample
+import { pipeline } from "@xenova/transformers"
+import {} from "./utils/transcription.js"
+
+export async function transcribe(audio) {
+  try {
+    console.log("Realizando a transcrição...")
+    const transcribe = await pipeline(
+      "automatic-speech-recognition",
+      "Xenova/whisper-small"
+    )
+    const transcription = await transcribe(audio, {
+      chunk_length_s: 30,
+      stride_length_s: 5,
+      language: "english",
+      task: "transcribe",
+    })
+    console.log("Transcrição finalizada com sucesso.")
+    return transcription?.text.replace("[Music]", "")
+  } catch (error) {
+    return new Error(error)
+  }
 }
