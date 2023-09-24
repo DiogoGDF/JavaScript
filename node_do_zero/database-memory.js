41fa45a9bdafa2a42812e40a58efbe404e3fd1d8
Diogo Garbinato <diogogarbinato@gmail.com>
2023-09-24 14:47:15 -0300

    update

---
+++ node_do_zero/database-memory.js
@@ -0,0 +1,22 @@
+import { randomUUID } from 'node:crypto'
+
+export class DatabaseMemory {
+    // # -> chave privada -> informação só visivel dentro da classe (como private do java)
+    #videos = new Map()
+
+    list() {
+        return this.#videos.values()
+    }
+
+    create(video) {
+        // UUID = Unique Universal ID
+        const videoID = randomUUID()
+        this.#videos.set(videoID, video)
+    }
+    update(id, video) {
+        this.#videos.set(id, video)
+    }
+    delete(id) {
+        this.#videos.delete(id)
+    }
+}
--- node_do_zero/server.js
+++ node_do_zero/server.js
@@ -1,8 +1,55 @@
-import { createServer } from "node:http"
+// import { createServer } from "node:http"
 
-const server = createServer(() => {
-  console.log("Hello, World!")
+// //request => dados que vem da requisição
+// //response => dados de resposta
+// const server = createServer((request, response) => {
+//   response.write("Hello, World!")
+//   return response.end()
+// })
+// server.listen(3333)
+
+//CRUD => Create, Read, Update, Delete
+
+// POST => método HTTP para criar um registro
+// PUT => ''  ''  para alteração
+
+import { fastify } from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
+
+const server = fastify()
+const database = new DatabaseMemory()
+
+// server.get('/') -> significa que quando o usuário digitar localhost:3333/ (sem nenhuma /asdfasdf adicional), irá retornar o método abstrato
+server.get('/', () => {
+  return 'Hello, World!'
+})
+server.get('/hello', () => {
+  return 'Hello, Fellow Cowboy!'
+})
+server.get('/node', () => {
+  return 'Hello, Node.js!'
 })
 
-server.listen(3333)
-//asdfasdfadsf
+// POST http://localhost:3333/videos => para criar um vídeo
+server.post('/videos', () => {
+  database.create({
+    title: 'Video 01',
+    description: 'Esse é o video 01',
+    duration: 180,
+  })
+  console.log(database.list())
+})
+server.get('/videos', () => {
+  return ''
+})
+// Rota para atualizar um video
+server.put('/videos/:id', () => {
+  return ''
+})
+server.delete('/videos/:id', () => {
+  return ''
+})
+
+server.listen({
+  port: 3333,
+})
