38c4b8312b8d9b2e19b867e8023d8a360fb64d9f
Diogo Garbinato <diogogarbinato@gmail.com>
2023-10-31 15:49:46 -0300

    get method

--- rocketseat/api/src/server.js
+++ rocketseat/api/src/server.js
@@ -3,6 +3,26 @@ const express = require('express')
 // express precisa ser inicializado
 const app = express()
 
+// get => uma requisição | quando alguém entrar na URL /, aquela será a resposta:
+app.get("/", (request, response) => {
+    response.send("Hello, world!")
+})
+
+// /:id -> parâmetro (desta forma são obrigatórios esses atributos para rodar a página)
+app.get("/message/:id/:user", (request, response) => {
+    response.send(`
+        Id da mensagem: ${request.params.id}.
+        Para o usuário: ${request.params.user}
+    `)
+})
+
+//o query é como os :id, mas sem ser obrigatório, e não precisa ser especificado como parametro ("/users/:page/:limit")
+app.get("/users", (request, response) => {
+    const { page, limit } = request.query
+
+    response.send(`Página: ${page}. Mostrar: ${limit}`)
+})
+
 // Porta a ser usada pelo express
 const PORT = 3333;
 
