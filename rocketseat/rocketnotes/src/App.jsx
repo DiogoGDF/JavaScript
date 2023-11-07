4d09d74949c0c4f4ad0ff6d5b37f28fc392b6467
Diogo Garbinato <diogogarbinato@gmail.com>
2023-11-07 17:00:25 -0300

    init

--- rocketseat/rocketnotes/src/App.jsx
+++
@@ -1,7 +0,0 @@
-// componentes sempre começam com letra maiuscula
-export default function App() {
-
-  return (
-    <h1>Hello, World!</h1>
-  )
-}
---
+++ rocketseat/rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles.js"
+import { Button } from "../../components/Button/index.jsx"
+
+export function Details(){
+
+  return (
+    <Container>
+      <h1>Hello, World!</h1>
+      <Button />
+    </Container>
+    
+  )
+}
