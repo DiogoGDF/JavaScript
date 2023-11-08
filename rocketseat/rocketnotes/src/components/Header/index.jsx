bd0688e68e020314c83f43115422ad5e9aba058f
Diogo Garbinato <diogogarbinato@gmail.com>
2023-11-07 22:20:46 -0300

    icons

--- rocketseat/rocketnotes/src/components/Header/index.jsx
+++ rocketseat/rocketnotes/src/components/Header/index.jsx
@@ -1,5 +1,5 @@
-import { Container } from "./styles";
-import { Profile } from "./styles";
+import { Container, Profile, Logout } from "./styles";
+import { RiShutDownLine } from 'react-icons/ri'
 
 export function Header(){
     return(
@@ -14,6 +14,9 @@ export function Header(){
                     <strong>Diogo Garbinato</strong>
                 </div>
             </Profile>
+            <Logout>
+                <RiShutDownLine />
+            </Logout>
         </Container>
     )
 }
