e501278ea4df44b3411177290d5817f1449ac35f
Diogo Garbinato <diogogarbinato@gmail.com>
2023-11-07 21:56:36 -0300

    Header

--- rocketseat/rocketnotes/src/pages/Details/index.jsx
+++ rocketseat/rocketnotes/src/pages/Details/index.jsx
@@ -1,10 +1,12 @@
 import { Container } from "./styles.js"
 import { Button } from "../../components/Button/index.jsx"
+import { Header } from "../../components/Header/index.jsx"
 
 export function Details(){
 
   return (
     <Container>
+      <Header />
       <h1>Hello, World!</h1>
       <Button title="Login" loading/>
       <Button title="Cadastrar"/>
