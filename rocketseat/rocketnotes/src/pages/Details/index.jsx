0f727c736de3f501cac3e31f0912cab04ba0de8e
Diogo Garbinato <diogogarbinato@gmail.com>
2023-11-08 15:40:52 -0300

    finalizando detalhes

--- rocketseat/rocketnotes/src/pages/Details/index.jsx
+++ rocketseat/rocketnotes/src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./styles.js"
+import { Container, Links, Content } from "./styles.js"
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
 import { Section } from "../../components/Section"
@@ -11,18 +11,36 @@ export function Details(){
   return (
     <Container>
       <Header />
-      <ButtonText title="Excluir nota" />
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-        </Links>
-      </Section>
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="node" />
-      </Section>
-      <Button title="Voltar"/>
+
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+
+          <h1>Introdução ao React</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Molestiae magni omnis libero tempora, dignissimos est! Natus, 
+            a iusto porro nemo quia nobis deserunt eius! 
+            Omnis sed dolorum ea odio praesentium!
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node" />
+          </Section>
+
+          <Button title="Voltar"/>
+
+        </Content>
+      </main>
+
     </Container>
     
   )
