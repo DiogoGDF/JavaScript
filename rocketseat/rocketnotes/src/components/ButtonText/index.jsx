4cc065f0b94767731a1c215916004dbce61be919
Diogo Garbinato <diogogarbinato@gmail.com>
2023-11-08 15:24:26 -0300

    ButtonText

---
+++ rocketseat/rocketnotes/src/components/ButtonText/index.jsx
@@ -0,0 +1,19 @@
+import { Container } from './styles'
+import PropTypes from 'prop-types'
+
+ButtonText.propTypes = {
+    title: PropTypes.string.isRequired,
+    isActive: PropTypes.bool
+}
+
+export function ButtonText({ title, isActive = false, ...rest }) {
+  return (
+    <Container
+      type="button"
+      $isactive={isActive.toString()}
+      {...rest}
+    >
+      {title}
+    </Container>
+  )
+}
--- rocketseat/rocketnotes/src/pages/Details/index.jsx
+++ rocketseat/rocketnotes/src/pages/Details/index.jsx
@@ -3,12 +3,15 @@ import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
 import { Section } from "../../components/Section"
 import { Tag } from "../../components/Tag"
+import { ButtonText } from '../../components/ButtonText'
+
 
 export function Details(){
 
   return (
     <Container>
       <Header />
+      <ButtonText title="Excluir nota" />
       <Section title="Links úteis">
         <Links>
           <li><a href="#">https://www.rocketseat.com.br</a></li>
@@ -16,7 +19,8 @@ export function Details(){
         </Links>
       </Section>
       <Section title="Marcadores">
-        <Tag />
+        <Tag title="express" />
+        <Tag title="node" />
       </Section>
       <Button title="Voltar"/>
     </Container>
