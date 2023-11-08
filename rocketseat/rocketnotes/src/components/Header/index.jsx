ef54242013f8c0f3fbc76ae4dfd68f908589c385
Diogo Garbinato <diogogarbinato@gmail.com>
2023-11-07 22:08:53 -0300

    img do usr

--- rocketseat/rocketnotes/src/components/Header/index.jsx
+++ rocketseat/rocketnotes/src/components/Header/index.jsx
@@ -1,7 +1,19 @@
 import { Container } from "./styles";
+import { Profile } from "./styles";
 
 export function Header(){
     return(
-        <Container></Container>
+        <Container>
+            <Profile>
+                <img 
+                    src="https://github.com/DiogoGDF.png"
+                    alt="Foto do usuário"
+                />
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>Diogo Garbinato</strong>
+                </div>
+            </Profile>
+        </Container>
     )
 }
