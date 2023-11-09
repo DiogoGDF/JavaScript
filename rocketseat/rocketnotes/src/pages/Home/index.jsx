7b0aeb5d1256c844952228120529c55c47d7a973
Diogo Garbinato <diogogarbinato@gmail.com>
2023-11-09 20:21:07 -0300

    notes

--- rocketseat/rocketnotes/src/pages/Home/index.jsx
+++ rocketseat/rocketnotes/src/pages/Home/index.jsx
@@ -4,6 +4,8 @@ import { FiPlus, FiSearch } from 'react-icons/fi'
 import { Header } from '../../components/Header'
 import { ButtonText } from '../../components/ButtonText'
 import { Input } from '../../components/Input'
+import { Section } from '../../components/Section'
+import { Note } from '../../components/Note'
 
 export function Home(){
     return (
@@ -25,7 +27,15 @@ export function Home(){
             </Search>
 
             <Content>
-
+                <Section title="Minhas notas">
+                    <Note data={{ 
+                        title: 'React', 
+                        tags: [
+                            {id: '1', name: 'react'},
+                            {id: '2', name: 'rocketseat'},
+                        ] 
+                    }}/>
+                </Section>
             </Content>
 
             <NewNote>
