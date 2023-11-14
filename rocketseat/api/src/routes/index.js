bf62e09ff58edf4eb029b7cee03fda79aafed9d8
Diogo Garbinato <diogogarbinato@gmail.com>
2023-11-14 20:50:40 -0300

    tags controller

--- rocketseat/api/src/routes/index.js
+++ rocketseat/api/src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require("express")
 
 const usersRouter = require("./users.routes")
 const notesRouter = require("./notes.routes")
+const tagsRouter = require("./tags.routes")
 
 const routes = Router()
 
 //agora nas rotas dentro do users.routes eu não preciso mais escrever /users, é automático
 routes.use("/users", usersRouter)
 routes.use("/notes", notesRouter)
+routes.use("/tags", tagsRouter)
 
 module.exports = routes
