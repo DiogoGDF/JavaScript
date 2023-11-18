02819c288a6a88deace4e1aeed8151c024911daa
Diogo Garbinato <diogogarbinato@gmail.com>
2023-11-17 22:10:13 -0300

    fix: signOut

--- rocketseat/rocketnotes/src/components/Header/index.jsx
+++ rocketseat/rocketnotes/src/components/Header/index.jsx
@@ -1,9 +1,17 @@
 import { Container, Profile, Logout } from "./styles";
 import { useAuth } from "../../hooks/auth";
 import { RiShutDownLine } from 'react-icons/ri'
+import { useNavigate } from "react-router-dom";
 
 export function Header(){
     const { signOut } = useAuth()
+    const navigate = useNavigate()
+
+    function handleSignOut(){
+        navigate('/')
+        signOut()
+    }
+
     return(
         <Container>
             <Profile to="/profile">
@@ -16,7 +24,7 @@ export function Header(){
                     <strong>Diogo Garbinato</strong>
                 </div>
             </Profile>
-            <Logout onClick={signOut}>
+            <Logout onClick={handleSignOut}>
                 <RiShutDownLine />
             </Logout>
         </Container>
