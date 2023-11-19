c398f166cf52164e72d70ca0f947313c7e02395d
Diogo Garbinato <diogogarbinato@gmail.com>
2023-11-19 17:22:03 -0300

    Setting up for deploy

--- rocketseat/api/src/server.js
+++ rocketseat/api/src/server.js
@@ -35,7 +35,7 @@ app.use(( error, request, response, next ) => {
 })
 
 // Porta a ser usada pelo express
-const PORT = 3333;
+const PORT = process.env.PORT || 3001;
 
 // express fica "observando a porta", e função a ser executada quando inicializar
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
