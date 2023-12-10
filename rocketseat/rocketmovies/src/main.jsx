23a8b2d4e9e18dba6c30da7b2e37d092cb4c4012
Diogo Garbinato <diogogarbinato@gmail.com>
2023-12-10 20:55:15 -0300

    Styles

--- rocketseat/rocketmovies/src/main.jsx
+++ rocketseat/rocketmovies/src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import theme from './styles/theme'
+import GlobalStyles from './styles/global'
+
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Details } from './pages/Details'
+import { ThemeProvider } from 'styled-components'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
