6466144eae545df00a72e5fdd86c39e1042b0239
Diogo Garbinato <diogogarbinato@gmail.com>
2023-12-10 17:57:38 -0300

    AppError

--- rocketseat/api-movies/src/controllers/UsersController.js
+++ rocketseat/api-movies/src/controllers/UsersController.js
@@ -1,7 +1,17 @@
+const AppError = require('../utils/AppError')
 class UsersController {
   create(req, res) {
     const { name, email, password } = req.body
-    res.json({ name, email, password })
+    if ( !name ) { 
+      throw new AppError('Nome é obrigatório')
+    }
+    else if ( !email ) { 
+      throw new AppError('Email é obrigatório')
+    }
+    else if ( !password ) { 
+      throw new AppError('Senha é obrigatória')
+    }
+    res.status(201).json({ name, email, password })
   }
 }
 
--- rocketseat/api-movies/src/server.js
+++ rocketseat/api-movies/src/server.js
@@ -1,15 +1,32 @@
+require('express-async-errors')
 const express = require('express')
 //const cors = require('cors')
 const routes = require('./routes')
+const AppError = require('./utils/AppError')
 
 const app = express()
 //app.use(cors)
 app.use(express.json())
 app.use(routes)
 
+app.use((error, req, res, next) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+  console.log(error)
+  return res.status(500).json({
+    status: 'error',
+    message: error.message
+  })
+})
+
 app.get('/', (req, res) => {
   res.send('Hello, World!')
 })
 
 const port = 3003
+
 app.listen(port, () => console.log(`Server is running on port ${port}`))
