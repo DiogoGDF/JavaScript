b05a1aa4492d2a73c502c0108707e3d2934a15ec
Diogo Garbinato <diogogarbinato@gmail.com>
2023-12-10 21:14:44 -0300

    home page init

--- rocketseat/rocketmovies/src/main.jsx
+++ rocketseat/rocketmovies/src/main.jsx
@@ -3,14 +3,14 @@ import theme from './styles/theme'
 import GlobalStyles from './styles/global'
 
 import ReactDOM from 'react-dom/client'
-import { Details } from './pages/Details'
 import { ThemeProvider } from 'styled-components'
+import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Details />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
