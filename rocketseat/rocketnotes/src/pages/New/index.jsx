f2c33c25c509ebc2ed75d3fe92b6de3188c2ac48
Diogo Garbinato <diogogarbinato@gmail.com>
2023-12-28 15:35:52 -0300

    refactor: better navigation

--- rocketseat/rocketnotes/src/pages/New/index.jsx
+++ rocketseat/rocketnotes/src/pages/New/index.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 import { Textarea } from "../../components/Textarea";
@@ -10,130 +10,138 @@ import { Input } from "../../components/Input";
 
 import { Container, Form } from "./styles";
 import { api } from "../../services/api";
+import { ButtonText } from "../../components/ButtonText";
 
 export function New() {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
 
-  const [links, setLinks] = useState([]);
-  const [newLink, setNewLink] = useState("");
+    const [links, setLinks] = useState([]);
+    const [newLink, setNewLink] = useState("");
 
-  const [tags, setTags] = useState([]);
-  const [newTag, setNewTag] = useState("");
+    const [tags, setTags] = useState([]);
+    const [newTag, setNewTag] = useState("");
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  function handleAddLink() {
-    setLinks((prevState) => {
-      if (!prevState.includes(newLink.trim())) {
-        return [...prevState, newLink.trim()];
-      }
-      return prevState;
-    });
-    setNewLink("");
-  }
-
-  function handleRemoveLink(deleted) {
-    setLinks((prevState) => prevState.filter((link) => link !== deleted));
-  }
-
-  function handleAddTag() {
-    setTags((prevState) => {
-      if (!prevState.includes(newTag.trim())) {
-        return [...prevState, newTag.trim()];
-      }
-      return prevState;
-    });
-    setNewTag("");
-  }
-
-  function handleRemoveTag(deleted) {
-    setTags((prevState) => prevState.filter((tag) => tag !== deleted));
-  }
-
-  async function handleNewNote(){
-    if (!title) {
-        return alert("Digite o título da nota!");
+    function handleBack(){
+        navigate(-1);
     }
-    if (newTag) {
-        return alert("Você não adicionou a tag");
+
+    function handleAddLink() {
+        setLinks((prevState) => {
+            if (!prevState.includes(newLink.trim())) {
+                return [...prevState, newLink.trim()];
+            }
+            return prevState;
+        });
+        setNewLink("");
     }
-    if (newLink) {
-        return alert("Você não adicionou o link");
+
+    function handleRemoveLink(deleted) {
+        setLinks((prevState) => prevState.filter((link) => link !== deleted));
     }
-    await api.post("/notes", {
-        title,
-        description,
-        tags,
-        links
-    });
 
-    alert("Nota criada com sucesso!");
-    navigate("/");
-  }
+    function handleAddTag() {
+        setTags((prevState) => {
+            if (!prevState.includes(newTag.trim())) {
+                return [...prevState, newTag.trim()];
+            }
+            return prevState;
+        });
+        setNewTag("");
+    }
 
-  return (
-    <Container>
-      <Header />
+    function handleRemoveTag(deleted) {
+        setTags((prevState) => prevState.filter((tag) => tag !== deleted));
+    }
 
-      <main>
-        <Form>
-          <header>
-            <h1>Criar nota</h1>
-            <Link to="/">voltar</Link>
-          </header>
+    async function handleNewNote(){
+        if (!title) {
+            return alert("Digite o título da nota!");
+        }
+        if (newTag) {
+            return alert("Você não adicionou a tag");
+        }
+        if (newLink) {
+            return alert("Você não adicionou o link");
+        }
+        await api.post("/notes", {
+            title,
+            description,
+            tags,
+            links
+        });
 
-          <Input
-            placeholder="Título"
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <Textarea 
-            placeholder="Observações" 
-            onChange={(e) => setDescription(e.target.value)}
-          />
+        alert("Nota criada com sucesso!");
+        navigate(-1);
+    }
 
-          <Section title="Links úteis">
-            {links.map((link, index) => (
-              <NoteItem
-                key={String(index)}
-                value={link}
-                onClick={() => handleRemoveLink(link)}
-              />
-            ))}
-            <NoteItem
-              isNew
-              placeholder="Novo link"
-              value={newLink}
-              onChange={(e) => setNewLink(e.target.value)}
-              onClick={handleAddLink}
-            />
-          </Section>
+    return (
+        <Container>
+            <Header />
 
-          <Section title="Marcadores">
-            <div className="tags">
-              {tags.map((tag, index) => (
-                <NoteItem
-                  key={String(index)}
-                  value={tag}
-                  onClick={() => handleRemoveTag(tag)}
-                />
-              ))}
-              <NoteItem
-                isNew
-                placeholder="Nova tag"
-                value={newTag}
-                onChange={(e) => setNewTag(e.target.value)}
-                onClick={handleAddTag}
-              />
-            </div>
-          </Section>
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar nota</h1>
+                        <ButtonText
+                            title="Voltar"
+                            onClick={handleBack}
+                        />
+                    </header>
 
-          <Button 
-            title="Salvar" 
-            onClick={handleNewNote}
-          />
-        </Form>
-      </main>
-    </Container>
-  );
+                    <Input
+                        placeholder="Título"
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <Textarea
+                        placeholder="Observações"
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+
+                    <Section title="Links úteis">
+                        {links.map((link, index) => (
+                            <NoteItem
+                                key={String(index)}
+                                value={link}
+                                onClick={() => handleRemoveLink(link)}
+                            />
+                        ))}
+                        <NoteItem
+                            isNew
+                            placeholder="Novo link"
+                            value={newLink}
+                            onChange={(e) => setNewLink(e.target.value)}
+                            onClick={handleAddLink}
+                        />
+                    </Section>
+
+                    <Section title="Marcadores">
+                        <div className="tags">
+                            {tags.map((tag, index) => (
+                                <NoteItem
+                                    key={String(index)}
+                                    value={tag}
+                                    onClick={() => handleRemoveTag(tag)}
+                                />
+                            ))}
+                            <NoteItem
+                                isNew
+                                placeholder="Nova tag"
+                                value={newTag}
+                                onChange={(e) => setNewTag(e.target.value)}
+                                onClick={handleAddTag}
+                            />
+                        </div>
+                    </Section>
+
+                    <Button
+                        title="Salvar"
+                        onClick={handleNewNote}
+                    />
+                </Form>
+            </main>
+        </Container>
+    );
 }
--- rocketseat/rocketnotes/src/pages/Profile/index.jsx
+++ rocketseat/rocketnotes/src/pages/Profile/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import { useAuth } from "../../hooks/auth";
 import { api } from "../../services/api";
@@ -13,93 +13,98 @@ import { Button } from "../../components/Button";
 import { Container, Form, Avatar } from "./styles";
 
 export function Profile() {
-  const { user, updateProfile } = useAuth();
+    const { user, updateProfile } = useAuth();
 
-  const [ name, setName ] = useState(user.name);
-  const [ email, setEmail ] = useState(user.email);
-  const [ passwordOld, setPasswordOld ] = useState();
-  const [ passwordNew, setPasswordNew ] = useState();
+    const [ name, setName ] = useState(user.name);
+    const [ email, setEmail ] = useState(user.email);
+    const [ passwordOld, setPasswordOld ] = useState();
+    const [ passwordNew, setPasswordNew ] = useState();
 
-  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+    const navigate = useNavigate();
 
-  const [ avatar, setAvatar ] = useState(avatarUrl);
-  const [ avatarFile, setAvatarFile ] = useState(null);
+    const [ avatar, setAvatar ] = useState(avatarUrl);
+    const [ avatarFile, setAvatarFile ] = useState(null);
 
-  async function handleUpdate(){
-    const user ={
-      name,
-      email,
-      password: passwordNew,
-      old_password: passwordOld
-    };
+    function handleBack(){
+        navigate(-1);
+    }
 
-    await updateProfile({ user, avatarFile });
-  }
+    async function handleUpdate(){
+        const user ={
+            name,
+            email,
+            password: passwordNew,
+            old_password: passwordOld
+        };
 
-  function handleChangeAvatar(event){
-    const file = event.target.files[0];
-    setAvatarFile(file);
-    const imagePreview = URL.createObjectURL(file);
-    setAvatar(imagePreview);
-  }
+        await updateProfile({ user, avatarFile });
+    }
 
-  return (
-    <Container>
-      <header>
-          <Link to="/">
-            <FiArrowLeft />
-          </Link>
-      </header>
+    function handleChangeAvatar(event){
+        const file = event.target.files[0];
+        setAvatarFile(file);
+        const imagePreview = URL.createObjectURL(file);
+        setAvatar(imagePreview);
+    }
 
-      <Form>
-        <Avatar>
-          <img
-            src={avatar}
-            alt="Foto do usuário"
-          />
-          <label htmlFor="avatar">
-            <FiCamera />
+    return (
+        <Container>
+            <header>
+                <button type="button" onClick={handleBack}>
+                    <FiArrowLeft />
+                </button>
+            </header>
 
-            <input
-              id="avatar"
-              type="file"
-              onChange={handleChangeAvatar}
-            />
-          </label>
-        </Avatar>
+            <Form>
+                <Avatar>
+                    <img
+                        src={avatar}
+                        alt="Foto do usuário"
+                    />
+                    <label htmlFor="avatar">
+                        <FiCamera />
 
-        <Input
-          placeholder="Nome"
-          type="text"
-          icon={FiUser}
-          value={name}
-          onChange={e => setName(e.target.value)}
-        />
+                        <input
+                            id="avatar"
+                            type="file"
+                            onChange={handleChangeAvatar}
+                        />
+                    </label>
+                </Avatar>
 
-        <Input
-          placeholder="E-mail"
-          type="text"
-          icon={FiMail}
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
+                <Input
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiUser}
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                />
 
-        <Input
-          placeholder="Senha atual"
-          type="password"
-          icon={FiLock}
-          onChange={e => setPasswordOld(e.target.value)}
-        />
+                <Input
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
 
-        <Input
-          placeholder="Nova senha"
-          type="password"
-          icon={FiLock}
-          onChange={e => setPasswordNew(e.target.value)}
-        />
+                <Input
+                    placeholder="Senha atual"
+                    type="password"
+                    icon={FiLock}
+                    onChange={e => setPasswordOld(e.target.value)}
+                />
 
-        <Button title="Salvar" onClick={handleUpdate}/>
-      </Form>
-    </Container>
-  );
+                <Input
+                    placeholder="Nova senha"
+                    type="password"
+                    icon={FiLock}
+                    onChange={e => setPasswordNew(e.target.value)}
+                />
+
+                <Button title="Salvar" onClick={handleUpdate}/>
+            </Form>
+        </Container>
+    );
 }
