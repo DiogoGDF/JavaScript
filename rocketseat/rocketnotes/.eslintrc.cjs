4e07896f6671992d8ff209d2c46c7b9ffdf7159a
Diogo Garbinato <diogogarbinato@gmail.com>
2023-12-28 12:44:03 -0300

    feat: eslint indent

--- rocketseat/rocketnotes/.eslintrc.cjs
+++ rocketseat/rocketnotes/.eslintrc.cjs
@@ -1,24 +1,24 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
     ],
-    'semi' : ["warn", "always"],
-    "quotes": ["warn", "double", { "avoidEscape": true, "allowTemplateLiterals": true }],
-    "no-multiple-empty-lines": ["warn", { "max": 1, "maxEOF": 1, "maxBOF": 0 }]
-
-  },
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    settings: { react: { version: '18.2' } },
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        'semi': ["warn", "always"],
+        "quotes": ["warn", "double", { "avoidEscape": true, "allowTemplateLiterals": true }],
+        "no-multiple-empty-lines": ["warn", { "max": 1, "maxEOF": 1, "maxBOF": 0 }],
+        "indent": ["warn", 4]
+    },
 }
